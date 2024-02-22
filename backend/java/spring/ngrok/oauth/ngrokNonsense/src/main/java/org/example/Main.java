a51b059bbb7e693b5e2c15a249d13ba955092bf4
james <james.dunsmore@baesystems.com>
2024-02-22 11:45:36 +0000

    added gitignore

--- backend/java/spring/ngrok/oauth/ngrokNonsense/src/main/java/org/example/Main.java
+++ backend/java/spring/ngrok/oauth/ngrokNonsense/src/main/java/org/example/Main.java
@@ -3,9 +3,10 @@ package org.example;
 import com.github.alexdlaird.ngrok.NgrokClient;
 import com.github.alexdlaird.ngrok.conf.JavaNgrokConfig;
 import com.github.alexdlaird.ngrok.protocol.CreateTunnel;
+import com.github.alexdlaird.ngrok.protocol.Tunnel;
+import com.github.alexdlaird.ngrok.protocol.TunnelOAuth;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,13 +14,20 @@ public class Main {
         JavaNgrokConfig ngrokConfig = new JavaNgrokConfig.Builder()
 
                 .withNgrokPath(Path.of("/usr/local/bin/ngrok"))
-                .withoutMonitoring()
                 .build();
         NgrokClient ngrokClient = new NgrokClient.Builder()
                 .withJavaNgrokConfig(ngrokConfig)
                 .build();
+        TunnelOAuth tunnelOAuth = new TunnelOAuth.Builder()
+                .withProvider("google")
+                .build();
         CreateTunnel createTunnel = new CreateTunnel.Builder()
+                .withAddr("http://localhost:8080")
+                .withOAuth(tunnelOAuth)
                 .build();
         ;
+        Tunnel tunnel = ngrokClient.connect(createTunnel);
+        String pubUrl = tunnel.getPublicUrl();
+        System.out.println(pubUrl);
     }
 }
