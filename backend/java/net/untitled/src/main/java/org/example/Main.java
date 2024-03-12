597202c70652020927a43298844f4ca4d070b437
james <james.dunsmore@baesystems.com>
2024-03-12 16:47:54 +0000

    added some java to net

--- backend/java/net/untitled/src/main/java/org/example/Main.java
+++ backend/java/net/untitled/src/main/java/org/example/Main.java
@@ -1,9 +1,64 @@
 package org.example;
 
 import java.io.*;
+import java.util.Arrays;
 
 
 public class Main {
+
+    private static String[] readFile(String file) throws IOException {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line = null;
+            StringBuilder stringBuilder = new StringBuilder();
+            StringBuilder unedited = new StringBuilder();
+            String ls = System.getProperty("line.separator");
+            while ((line = reader.readLine()) != null) {
+                unedited.append(line);
+                unedited.append(ls);
+                line = formatLineToJson(line);
+                stringBuilder.append(line);
+            }
+            return new String[]{unedited.toString() , stringBuilder.toString()};
+        }
+    }
+    public static String formatToJson(String raw) {
+        if (raw != null) {
+            String[] splitRaw = raw.split("\n");
+            raw = "{'" + raw + "',}\n";
+            //replace connected '*' with TRUE, blank with FALSE
+            raw = raw.replaceAll("\\*:","TRUE:")
+                    .replaceAll("\\s:", "FALSE:")
+                    //replace address ':' with '@' for meantime
+                    .replaceAll("\\\\:","@")
+                    //replace ':' delimiter with valid json delimit
+                    .replaceAll(":","','")
+                    //sub ':' back in for '_' placeholder
+                    .replaceAll("@",":");
+
+            return raw;
+        }
+        return null;
+    }
+
+    public static String formatLineToJson(String line) {
+        if (line != null) {
+            line = "{'" + line + "',}\n";
+            //replace connected '*' with TRUE, blank with FALSE
+            line = line.replaceAll("\\*:","TRUE:")
+                    .replaceAll("\\s:", "FALSE:")
+                    //replace address ':' with '@' for meantime
+                    .replaceAll("\\\\:","@")
+                    //replace ':' delimiter with valid json delimit
+                    .replaceAll(":","','")
+                    //sub ':' back in for '_' placeholder
+                    .replaceAll("@",":");
+
+            return line;
+        }
+        return null;
+    }
+
 
 public static String getOutput() throws IOException, InterruptedException {
     Runtime rt = Runtime.getRuntime();
@@ -19,14 +74,20 @@ public static String getOutput() throws IOException, InterruptedException {
 
             line = "'" + line + "'";
             //replace connected '*' with TRUE, blank with FALSE
-            line = line.replaceAll("\\*:","TRUE:");
-            line = line.replaceAll("\\s:", "FALSE:");
-            //replace address ':' with '_' for meantime
-            line = line.replaceAll("\\\\:","_");
-            //replace ':' delimiter with valid json delimit
-            line = line.replaceAll(":","','");
-            //sub ':' back in for '_' placeholder
-            line = line.replaceAll("_",":");
+            line = line.replaceAll("\\*:","TRUE:")
+                    .replaceAll("\\s:", "FALSE:")
+                    //replace address ':' with '@' for meantime
+                    .replaceAll("\\\\:","@")
+                    //replace ':' delimiter with valid json delimit
+                    .replaceAll(":","','")
+                    //sub ':' back in for '_' placeholder
+                    .replaceAll("@",":");
+
+
+
+
+
+
             System.out.println(line);
             s.append(stdOutput.readLine());
         }
@@ -35,7 +96,9 @@ public static String getOutput() throws IOException, InterruptedException {
 }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        getOutput();
-    //System.out.println(getOutput());
+    String[] res = readFile("thisIsATestFile");
+    String raw = res[0];
+
+
     }
 }
